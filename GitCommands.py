bd62523327b663b6d682d50b6a6cc4ab7e60b975
Thiru89 <Thiru89>
2024-09-19 00:14:28 +0000

    adding book2

---
+++ GitCommands.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+!git fetch origin
+
+# COMMAND ----------
+
+!git checkout main
+
+
+# COMMAND ----------
+
+git pull origin main
+
+
+# COMMAND ----------
+
+git merge featurebranch1
+
+# COMMAND ----------
+
+git push origin main
